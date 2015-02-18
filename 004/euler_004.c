0cc6994debd96a44ec68a778b418dd791ab2177d
Pär Strindevall <par@strindevall.com>
2015-02-19 00:29:50 +0100

    Solved problem 004.

---
+++ 004/euler_004.c
@@ -0,0 +1,44 @@
+#import <stdio.h>
+
+int isPalindrome(int candidate);
+int largestPalindrome();
+
+int main() {
+	printf("The largest palindromic number made from the product of two 3-digit numbers is %d\n", largestPalindrome());
+}
+
+int largestPalindrome() {
+	int largestPalindrome;
+	int x = 999;
+	int y = 1;
+	while (x) {
+		while (y < 1000) {
+			int candidate = isPalindrome(x * y);
+			if (candidate && x * y > largestPalindrome) {
+				largestPalindrome = x * y;
+			}
+			y++;
+		}
+		y = 1;
+		x--;
+	}
+	return largestPalindrome;
+}
+
+int isPalindrome(int candidate) {
+	int digits[6]; // Can become at most six digits since 999 * 999 = 998001
+	int x = 0, y = 0;
+	while (candidate) {
+		digits[x++] = candidate % 10;
+		candidate /= 10;
+	}
+	x--;
+	while (x > y) {
+		if (digits[x] != digits[y]) {
+			return 0;
+		}
+		x--;
+		y++;
+	}
+	return 1;
+}
